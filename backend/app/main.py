5fbd706c9e78196980e46c31779a7e8a6e07b451
kasarla-gopichand <kasarlagopichand24@gmail.com>
2026-07-07 12:37:24 +0530

    Worked on Appointment Booking

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from app.routers import doctors
+from app.routers import slots
 app = FastAPI()
 app.include_router(doctors.router)
+app.include_router(slots.router)
 
 from app.database import Base, engine
 from app.models import doctor
 from app.routers import doctor
 
-app = FastAPI()
+
 application = app
 
 app.include_router(doctor.router)
