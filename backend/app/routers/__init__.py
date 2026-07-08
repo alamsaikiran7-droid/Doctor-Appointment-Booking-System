d1f70067f475d38489e7049bd54e70926db884c7
Sai Kiran Alam <alamsaikiran7@gmail.com>
2026-07-08 15:12:54 +0530

    "Made some changes in Authentication"

---
+++ backend/app/routers/__init__.py
@@ -0,0 +1,13 @@
+from app.routers import auth
+from app.routers import admin
+from app.routers import doctors
+from app.routers import slots
+from app.routers import appointments
+
+__all__ = [
+    "auth",
+    "admin",
+    "doctors",
+    "slots",
+    "appointments",
+]
